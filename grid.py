25a8ca8b8307c639e54978d5a3c095aca860c1a2
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-07-22 09:54:32 +0200

    ajout de la méthode update dans la class Grid

--- grid.py
+++ grid.py
@@ -10,3 +10,19 @@ class Grid:
             for _ in range(10):
                 row.append(".")
             self.cells.append(row)
+
+    def update(self, shot: str, is_hit: bool) -> None:
+        """
+        Met à jour la grille après un tir.
+        :param shot: Coordonnées du tir
+        :param is_hit: True si le tir touche un bateau, False sinon.
+        :return:
+        """
+        column = shot[0]
+        row = int(shot[1:])
+        column_index = ord(column) - ord('A')
+        row_index = row - 1
+        if is_hit:
+            self.cells[row_index][column_index] = "O"
+        else:
+            self.cells[row_index][column_index] = "X"
