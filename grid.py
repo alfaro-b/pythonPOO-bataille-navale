11e7f2a2f4105cd3501c196d2ddac5373953e5cd
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-07-22 11:06:27 +0200

    Ajout de la méthode mark_sunk dans la class Grid

--- grid.py
+++ grid.py
@@ -1,3 +1,5 @@
+from boat import Boat
+
 class Grid:
 
     def __init__(self) -> None:
@@ -35,4 +37,16 @@ class Grid:
         for index, row in enumerate(self.cells):  # permet d'avoir le numéro de la ligne index+1 et ligne elle-même
             print(f"{index + 1:2} {' '.join(row)}")
             # dans les f string :2 permet de réserver deux caractères pour aligner
+
+    def mark_sunk(self, boat: Boat) -> None:
+        """
+        Met à jour la grille lorsqu'un bateau est coulé, marque les cases du bateau coulé en C.
+        :param boat: Le bateau qui vient d'être coulé.
+        """
+        for position in boat.positions:
+            column = position[0]
+            row = int(position[1:])
+            column_index = ord(column) - ord('A')
+            row_index = row - 1
+            self.cells[row_index][column_index] = "C"
 
