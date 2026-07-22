410cb56a90aaf5d3a9f04aeff2548a27f06ed432
Bittori Alfaro <bittori.alfaro@fms-ea.com>
2026-07-22 13:01:09 +0200

    ajout de la méthode ask_shot à la classe Game

--- game.py
+++ game.py
@@ -16,3 +16,39 @@ class Game:
         ]
         self.grid: Grid = Grid()
         self.misses: list[str] = []
+        self.shots: list[str] = []
+
+    def ask_shot(self) -> str:
+        """
+        Demande au joueur de saisir les coordonnées d'un tir.
+        :return: Les coordonnées valides saisies par le joueur.
+        """
+        while True:
+            coordinate_shot: str = input("Quelle case jouez-vous? Réponse format A1, par ex. ").upper()
+
+            # Vérifie validité
+            if len(coordinate_shot) < 2 or len(coordinate_shot) > 3:
+                print("Saisie invalide.")
+                continue
+
+            # Vérifie la colonne
+            if coordinate_shot[0] not in "ABCDEFGHIJ":
+                print("La colonne doit être comprise entre A et J.")
+                continue
+
+            # Vérifier la ligne
+            if not coordinate_shot[1:].isdigit():
+                print("Le numéro de ligne est invalide.")
+                continue
+
+            row = int(coordinate_shot[1:])
+            if row < 1 or row > 10:
+                print("La ligne doit être comprise entre 1 et 10.")
+                continue
+
+            # Vérifie si la case a déjà été jouée
+            if coordinate_shot in self.shots:
+                print("Cette case a déjà été jouée.")
+                continue
+
+            return coordinate_shot
